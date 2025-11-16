40c71a73b2d05d4c3eced1efe478e801222428c9
Uvarov Vladimir <uvarov_v_s@mail.ru>
2025-11-16 10:41:48 +0700

    aksdf;lkjasd;lfkja;sdlkfja;sdlkfj asdfgadfgsdfg

--- src/main/java/com/dreamteam/chaosgame/api/ApiController.java
+++ src/main/java/com/dreamteam/chaosgame/api/ApiController.java
@@ -14,12 +14,31 @@ public class ApiController {
     private final int counter =5;
 
     /**
+     *
+     *
      * @param infoId
      * @param type
      * @return
      */
     @GetMapping("/info/{infoId}")
     public ResponseDTO getInfo(@PathVariable("infoId") String infoId,
+                               @RequestParam(name = "type", required = false) String type) {
+
+        ResponseDTO responseDTO = new ResponseDTO();
+        responseDTO.setInfo("Hello World!!!!!!!!!!!");
+        responseDTO.setVersion("0.0.1");
+        responseDTO.setInfoId(infoId);
+        responseDTO.setType(type);
+
+        /*
+        asdfasdf
+         */
+
+        return responseDTO;
+    }
+
+
+    public ResponseDTO getInfo2(@PathVariable("infoId") String infoId,
                                @RequestParam(name = "type", required = false) String type) {
 
         ResponseDTO responseDTO = new ResponseDTO();
