82cdd1ced78352b80b1c51038babc3a50e702e51
1 <melentevse@gmail.com>
2025-11-16 10:37:08 +0700

    dasdasdas

--- src/main/java/com/dreamteam/chaosgame/ChaosgameApplication.java
+++ src/main/java/com/dreamteam/chaosgame/ChaosgameApplication.java
@@ -3,9 +3,15 @@ package com.dreamteam.chaosgame;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ */
 @SpringBootApplication
 public class ChaosgameApplication {
 
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(ChaosgameApplication.class, args);
 	}
