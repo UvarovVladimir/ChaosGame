1a027b0f569f16704ad594b40a5e7962f5b9e489
Normolist <normolist@gmail.com>
2026-02-25 11:42:17 +0700

    Добавлен комментарий, чтобы понять где какой проект.

--- src/main/java/com/dreamteam/chaosgame/ChaosgameApplication.java
+++ src/main/java/com/dreamteam/chaosgame/ChaosgameApplication.java
@@ -2,7 +2,7 @@ package com.dreamteam.chaosgame;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Normolist
 @SpringBootApplication
 public class ChaosgameApplication {
 
