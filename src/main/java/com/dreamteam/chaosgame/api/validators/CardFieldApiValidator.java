318811107c9b50e819d273df644ff23bd7937af3
1 <melentevse@gmail.com>
2026-02-28 23:29:59 +0700

    #14 Реализовать patch запрос на обновление полей карты

--- src/main/java/com/dreamteam/chaosgame/api/validators/CardFieldApiValidator.java
+++ src/main/java/com/dreamteam/chaosgame/api/validators/CardFieldApiValidator.java
@@ -1,4 +1,8 @@
 package com.dreamteam.chaosgame.api.validators;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CardFieldApiValidator {
+    public void fieldValidator ()
 }
