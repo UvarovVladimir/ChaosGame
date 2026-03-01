bed8f9e5859d9cfd07967a2dffa31bda32736bb1
1 <melentevse@gmail.com>
2026-03-01 11:36:54 +0700

    Revert "#14 Реализовать patch запрос на обновление полей карты"
    This reverts commit 318811107c9b50e819d273df644ff23bd7937af3.

--- src/main/java/com/dreamteam/chaosgame/api/validators/CardFieldApiValidator.java
+++ src/main/java/com/dreamteam/chaosgame/api/validators/CardFieldApiValidator.java
@@ -1,8 +1,4 @@
 package com.dreamteam.chaosgame.api.validators;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public class CardFieldApiValidator {
-    public void fieldValidator ()
 }
